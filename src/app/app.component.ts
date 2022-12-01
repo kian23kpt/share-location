2b0c1479c6713edc0901d141d3e68c5512d866e1
kian23kpt <kian23kpt@gmail.com>
2022-12-01 12:09:36 +0330

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { tileLayer, latLng } from 'leaflet';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'share-location';
+}
